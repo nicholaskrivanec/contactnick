3fb4a14b0889446c4541d180dccad732131422f4
Nicholas Krivanec <nickkrivanec@gmail.com>
2025-07-11 14:42:04 -0600

    Add files via upload
    MORRRE FILES

---
+++ client/src/components/Footer.vue
@@ -0,0 +1,9 @@
+<template>
+  <footer class="footer">
+    <a href="https://www.w3schools.com/spaces"
+      title="This website was made with W3Schools Spaces. Make your own free website today!" target="_blank">
+      <img src="https://spaces.w3schools.com/logo_4x.png"
+        alt="This website was made with W3Schools Spaces. Make your own free website today!" width="50" height="50" />
+    </a>
+  </footer>
+</template>
---
+++ client/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Footer from "../components/Footer.vue";
+import ContactForm from "../components/ContactForm.vue";
+</script>
+
+<template>
+  <ContactForm />
+  <Footer />
+</template>
